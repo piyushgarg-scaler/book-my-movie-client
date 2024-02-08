3828d46e76886039539982ac7f9a71779967c84d
Piyush Garg <piyushgarg.dev@gmail.com>
2024-02-08 20:59:11 +0530

    Added Req Interceptor

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,11 @@ import axios from 'axios'
 export const apiV1Instance = axios.create({
     baseURL: 'http://localhost:8000/api/v1/',
     responseType: 'json',
-    headers: {
-        Authorization: localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : undefined
-    }
+})
+
+apiV1Instance.interceptors.request.use((request) => {
+    const token = localStorage.getItem('token')
+    if (token)
+        request.headers.Authorization = `Bearer ${token}`
+    return request
 })
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,13 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.jsx";
 import "./index.css";
 
-const queryClient = new QueryClient({});
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
